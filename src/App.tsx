865e3c79431d1a5034cd2d6512acba1668c260c4
Ana Paula Moraes <ana@anawd.com>
2022-03-30 20:00:56 -0300

    Include GA

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import { BrowserRouter } from 'react-router-dom'
 
 import Routes from './routes';
 
 import GlobalStyle from './assets/styles/global'
 import { ApplicationProvider } from "./context/application";
+
+ReactGA.initialize('UA-114594548-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
